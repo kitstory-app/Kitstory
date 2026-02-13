e1522c1cdb53ce06ad1d445aa6a649128203faea
Kuro <hello@kurojifusky.com>
2026-02-13 09:31:57 +0800

    more plugin stuff

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,19 +1,46 @@
 use tauri::{
     menu::{MenuBuilder, MenuItemBuilder},
-    tray::TrayIconBuilder,
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Manager,
 };
-
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri_plugin_deep_link::DeepLinkExt;
 
 pub fn run() {
     tauri::Builder::default()
-        // Plugins
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_http::init())
+        // The persisted-scope plugin must be registered and initialized after the fs plugin
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_persisted_scope::init())
+        .plugin(tauri_plugin_websocket::init())
+        .plugin(tauri_plugin_autostart::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_positioner::init())
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        //
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
+        // Deep links
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.deep_link().register("kitstory")?;
+
+            #[cfg(any(windows, target_os = "linux"))]
+            {
+                use tauri_plugin_deep_link::DeepLinkExt;
+                app.deep_link().register_all()?;
+            }
+
+            Ok(())
+        })
+        // Menu stuff
         .setup(|app| {
             let menu = MenuBuilder::new(app)
                 /* This is where the quick access stuff in this area usually resides, but will be dynamically added via JS */
@@ -44,6 +71,21 @@ pub fn run() {
             TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
+                 .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {}
+                })
                 .on_menu_event(|app, event| match event.id().as_ref() {
                     "exit_app" => {
                         // TODO: Might need to do some checks such as unsaved file changes or some shit
